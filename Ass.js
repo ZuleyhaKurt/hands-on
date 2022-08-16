10329379aefc2620c72e21771fdc3c0c74a7d1d5
Züleyha Kurt <netice2017@gmail.com>
2022-08-16 18:48:45 +0300

    js work

---
+++ Ass.js
@@ -0,0 +1,73 @@
+// ! Calculator
+// let number1= +prompt("enter number 1");
+// let operation=prompt("enter an operation(+,-,*,/)");
+// let number2 =+prompt("enter number 2");
+
+// if (operation == "+"){
+//     let result=number1+number2;
+//     console.log(`Sayıların toplamı ${result} dır`)
+// }
+// else if (operation == "-"){
+//      result=number1-number2;
+//     console.log(`Sayıların farkı ${result} dır`)
+// }
+// else if (operation == "*"){
+//      result=number1*number2;
+//     console.log(`Sayıların çarpımı ${result} dır`)
+// }
+// else if (operation == "/"){
+//      result=number1/number2;
+//     console.log(`Sayıların bölümü ${result} dır`)
+// }
+// 
+// 
+// !Clarusway programs
+// let day=prompt("enter a day" );
+// switch(day){
+//     case "monday":
+//     case "tuesday":
+//     case "wednesday":
+//     case "thursday":
+//         console.log(`your schedule:InClass`)
+//         break;
+//     case "friday":
+//         console.log(`your schedule: Teamwork`)
+//         break;
+//     case "saturday":
+//         console.log(`your schedule: InClass and Workshop`)
+//         break;
+//     case "sunday":
+//         console.log(`your schedule: SelfStudy`)
+//         break;
+//     default:
+//         console.log(`enter a valid day`)
+//         break;
+// }
+
+//  !Salary raise
+// alert("welcome ,our dear teammate");
+// let salary=prompt("please enter your salary in TL");
+// let newSalary
+// if (salary<=5000){
+//     newSalary=1.5*5000;
+//     alert(`Congrats , we give a %50 raise `);
+//     console.log(`your new salary:${newSalary} TL`);
+// }
+//  else if (salary>5000){
+//     newSalary=1.1*5000;
+//     alert(`Congrats , we give a %10 raise `);
+//     console.log(`your new salary:${newSalary} TL`);
+// }
+
+// !Credit
+ alert("welcome to Akbank ");
+ alert("we will be happy to give you a credit")
+ let income= prompt("enter your monthly income please")
+ let expense= prompt("enter your monthly expense please ")
+
+ if (income-expense >=5000){
+     console.log(`Awesome, We can give you a credit `)
+ }
+ else {
+   console.log("we're sorry , we cannot give you credit")
+ }
