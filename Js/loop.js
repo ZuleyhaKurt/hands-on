7baaca361d597c28aa7316d180fdc63762b9ba9e
Züleyha Kurt <netice2017@gmail.com>
2022-08-17 20:57:53 +0300

    math round

--- Js/loop.js
+++ Js/loop.js
@@ -1,3 +1,4 @@
+// !n e kadar olan sayıları topla
 // const n= Number(prompt("bir sayı giriniz:"))
 
 // let total=0;
@@ -7,3 +8,16 @@
 // }
 
 // console.log(`totals number : ${total}`)
+
+// ! 0ile 100 arasında 10 tane tam sayı yazdır
+
+for(let i =1; i<=10 ;i++){
+    const rasgele= Math.round(Math.random()*100)
+    console.log(rasgele)
+}
+
+//* NOT:
+//* Math.floor() =>surekli asagiya yuvarlar
+//* Math.ceil() =>sürekli yukariya yuvarlar
+//* Math.round() => degerine gore yuvarlar.
+//* Math.trunc() => kesirli sayinin tam kismini alir.
