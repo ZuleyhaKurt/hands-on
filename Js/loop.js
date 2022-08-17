ee2d45be46f56a10ade83e4b382b6a8dbc372d58
Züleyha Kurt <netice2017@gmail.com>
2022-08-17 22:43:36 +0300

    do while

--- Js/loop.js
+++ Js/loop.js
@@ -39,15 +39,27 @@
 //     console.log(`${sayi} ${sonuc}`)
 // }
 
-console.log("****** WHILE *******");
+// !console.log("****** WHILE *******");
 
 //? ORNEK: Kullanicidan 0-100 arasinda bir not isteyen ve girilen not
 //? 0-100'den farkli ise Kullaciniyi uyararak yeniden 0-100 arasinda
 //? not girmeye zorlayan kodu while dongusu ile yaziniz.
 
-let not= +prompt ("lütfen 0 ile 100 arasında bir not giriniz")
-while(not<0 || not>100){
-    console.log("not 0 ile 100 arasında olmalıdır")
-    not= +prompt ("lütfen 0 ile 100 arasında bir not giriniz")
-}
-console.log("girdiğiniz not:",not)
+// let not= +prompt ("lütfen 0 ile 100 arasında bir not giriniz")
+// while(not<0 || not>100){
+//     console.log("not 0 ile 100 arasında olmalıdır")
+//     not= +prompt ("lütfen 0 ile 100 arasında bir not giriniz")
+// }
+// console.log("girdiğiniz not:",not)
+
+// ! DO WHİLE DÖNGÜSÜ
+
+let not1; 
+do{
+    not1= +prompt ("lütfen 0 ile 100 arasında bir not giriniz")
+    if(not1<0 || not1>100){
+        console.log("not 0 ile 100 arasında olmalıdır")
+    }
+}while(not1<0 || not1>100)
+
+console.log("girdiğiniz not:",not1)
