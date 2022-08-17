beca79d3c8298dafc9475a88d154a41e5f5eb101
Züleyha Kurt <netice2017@gmail.com>
2022-08-17 21:47:29 +0300

    asal sayı

--- Js/loop.js
+++ Js/loop.js
@@ -11,13 +11,30 @@
 
 // ! 0ile 100 arasında 10 tane tam sayı yazdır
 
-for(let i =1; i<=10 ;i++){
-    const rasgele= Math.round(Math.random()*100)
-    console.log(rasgele)
-}
+// for(let i =1; i<=10 ;i++){
+//     const rasgele= Math.round(Math.random()*100)
+//     console.log(rasgele)
+// }
 
 //* NOT:
 //* Math.floor() =>surekli asagiya yuvarlar
 //* Math.ceil() =>sürekli yukariya yuvarlar
 //* Math.round() => degerine gore yuvarlar.
 //* Math.trunc() => kesirli sayinin tam kismini alir.
+
+
+//! dışardan girilen sayının  asal olup olmadığını bul
+const sayi =Number(prompt("pozitif bir sayı giriniz"));
+let asal=true;
+if(sayi<=1){
+    alert("sayı 1'den büyük olmalıdır")
+}else{
+    for(let i=2; i<sayi; i++){
+       if(sayi % i ==0) {
+        asal=false;
+        break;
+       }
+    }
+    const sonuc=asal? "Asaldır" :"Asal Değildir"
+    console.log(`${sayi} ${sonuc}`)
+}
