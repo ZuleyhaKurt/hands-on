7014955ff508587b33537be68d430de7a19694c0
Züleyha Kurt <netice2017@gmail.com>
2022-08-17 20:42:43 +0300

    n e kadar toplama

---
+++ Js/loop.js
@@ -0,0 +1,9 @@
+// const n= Number(prompt("bir sayı giriniz:"))
+
+// let total=0;
+// for(let i=1; i<=n; i++){
+//     total+=i
+//     console.log(total,i)
+// }
+
+// console.log(`totals number : ${total}`)
