f932236f553dba5e96780903d02f235007b529ed
Züleyha Kurt <netice2017@gmail.com>
2022-08-18 18:38:03 +0300

    finish

--- Js/ass2.js
+++ Js/ass2.js
@@ -1,4 +1,4 @@
-
+// ! ÖDEV 1 
 let sayac=0;
 let total=0;
 let not1;
@@ -16,3 +16,38 @@ do{
 
 let ortalama=total/sayac;
 console.log(`Sınıfın ortalaması: ${ortalama}`)
+
+// ! ÖDEV 2
+
+while(true){
+
+    let hak = 5;
+    const rastgele = Math.round(Math.random() * 100);
+    console.log(rastgele);
+    let tahmin;
+
+     do {
+    tahmin = Number(prompt("Lutfen 0-100 arasinda bir tahmin giriniz:"));
+    hak -= 1;
+    if (tahmin === rastgele) {
+    console.log(`Tebrikler ${5 - hak} kerede bildiniz.`);
+    break;
+    } else if (tahmin < rastgele) {
+    console.log("ARTTIR ⬆️");
+    } else {
+    console.log("AZALT ⬇️");
+    }
+    } while (hak > 0);
+
+    if (tahmin !== rastgele) {
+    console.log("Uzgunuz oyunu kaybettiniz 😔😔");
+    }
+    
+    let devam= prompt( "Tekrar oynamak ister misiniz E/H?");
+    if(devam==="H"||devam ==="h"){
+        break;
+    }
+    
+}
+
+
