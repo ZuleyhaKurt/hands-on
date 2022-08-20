a4deadfccd7d68c7ef41ba729212714ed9bc991e
Züleyha Kurt <netice2017@gmail.com>
2022-08-20 12:40:26 +0300

    new date

--- Js/function.js
+++ Js/function.js
@@ -6,3 +6,12 @@ function selamla(ad,soyad=""){
 selamla("Can","Yılmaz");
 selamla("Canan","Öztürk")
 selamla("Ayşe")
+
+function yasHesapla(isim,dogumTarihi){
+//  const sonuc=`${isim} 'in yaşı ${2022-dogumTarihi}`
+const sonuc=`${isim} 'in yaşı ${ new Date().getFullYear() - dogumTarihi} dir`
+ return sonuc;
+}
+
+const mesaj1= yasHesapla("züü",1993);
+console.log(mesaj1);
