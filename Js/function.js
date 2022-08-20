670f8c9bac7f3e5c19fa632b292b71abb05478e4
Züleyha Kurt <netice2017@gmail.com>
2022-08-20 12:27:39 +0300

    default parametre

--- Js/function.js
+++ Js/function.js
@@ -1,6 +1,8 @@
-function selamla(ad,soyad){
+// default parametre
+function selamla(ad,soyad=""){      
     console.log(`Merhaba ${ad} ${soyad}`);
 
 }
 selamla("Can","Yılmaz");
 selamla("Canan","Öztürk")
+selamla("Ayşe")
