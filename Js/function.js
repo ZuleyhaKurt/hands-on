da589c7da52e115ecea91d58023cf1a7894555f0
Züleyha Kurt <netice2017@gmail.com>
2022-08-20 13:19:52 +0300

    tek çift

--- Js/function.js
+++ Js/function.js
@@ -19,4 +19,10 @@ console.log(mesaj1);
 
 // console.log(yasHesapla("Veli Canan",1990)
 
-funct
+function tekCift(sayi){
+    return sayi % 2 ? ` ${sayi} TEKTİR`: `${sayi} CİFTTİR`;
+}
+const sayi= +prompt("bir sayı giriniz")
+console.log(tekCift(5)) 
+console.log(tekCift(2))
+console.log(tekCift(sayi))
