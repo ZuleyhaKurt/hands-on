3ab57343731378c00f467e1b6645860045313c01
Züleyha Kurt <netice2017@gmail.com>
2022-08-20 12:51:54 +0300

    going on

--- Js/function.js
+++ Js/function.js
@@ -11,7 +11,12 @@ function yasHesapla(isim,dogumTarihi){
 //  const sonuc=`${isim} 'in yaşı ${2022-dogumTarihi}`
 const sonuc=`${isim} 'in yaşı ${ new Date().getFullYear() - dogumTarihi} dir`
  return sonuc;
+// console.log(`${isim} 'in yaşı ${2022-dogumTarihi}`)
 }
 
 const mesaj1= yasHesapla("züü",1993);
 console.log(mesaj1);
+
+// console.log(yasHesapla("Veli Canan",1990)
+
+funct
