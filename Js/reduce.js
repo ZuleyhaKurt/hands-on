5ee7a60aca32c59951ad5be205acc73b0e322382
Züleyha Kurt <netice2017@gmail.com>
2022-09-05 21:31:54 +0300

    edit

--- Js/reduce.js
+++ Js/reduce.js
@@ -2,3 +2,18 @@ const salaries =[5500,8000,6500,9000,10000,15000,25000];
 
 const sumOfSalaries=salaries.reduce((acc,val)=>acc+val,0)
 console.log("sum:",sumOfSalaries)
+
+
+// firma 9000 den az olan kişilere %10 zam yapmak istiyor . zamlı halleriyle toplam kaç tl verildiğini bulan programı yaz
+
+const worker={
+    ad:"Can",
+    surname:"Canan",
+    age:33,
+    job :"developer",
+    language:["c++","Java","JavaScript","Go"],
+    salary:140000,
+}
+console.log(worker);
+console.log(worker.job)
+console.log(worker["language"])
