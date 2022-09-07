7f836a44145920639dc57ff8f04c75403fee4376
Züleyha Kurt <netice2017@gmail.com>
2022-09-07 21:46:33 +0300

    edit

--- Js/object.js
+++ Js/object.js
@@ -10,3 +10,21 @@ team.forEach((person)=> console.log(person.job ))
 const ages=team.map((p)=>p.age+1)
 console.log(ages)
 
+const teamFullName=team.map((p)=>({
+    fullName:p.name.toUpperCase() + " " + p.surname.toUpperCase(),
+    age:p.age + 5,
+})
+)
+  console.log(teamFullName) 
+
+   team.filter(p => p.age <= 22).forEach(p => console.log(p.name))
+
+////
+
+const teamUnder22=team.filter((x)=> x.age <=22).map(p => p.name)
+console.log(teamUnder22)
+
+const avgAges= team.reduce((sum,person)=> sum +=person.age ,0) / team.length;
+console.log(avgAges)
+
+
